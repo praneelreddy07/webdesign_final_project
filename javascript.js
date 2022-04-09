0750b57afaf795e7d97bd4ef6eb21e6edf92ba5e
praneelreddy07 <praneelreddy0509@gmail.com>
2022-04-08 20:39:59 -0400

    First Commit

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function ach(){
+    document.getElementById('con').innerHTML = "University Gold Medalist in CS"
+}
+function hob(){
+    document.getElementById('con').innerHTML = "Developing New Websites and Coding"
+}
+function vol(){
+    document.getElementById('con').innerHTML = "Did Voluntary work in blood donation camps"
+}
