0436cb83dc4d178ca62c43440eb954901b3fc89c
praneelreddy07 <praneelreddy0509@gmail.com>
2022-04-25 18:13:28 -0400

    Third Phase commit 1

--- javascript.js
+++ javascript.js
@@ -7,3 +7,28 @@ function hob(){
 function vol(){
     document.getElementById('con').innerHTML = "Did Voluntary work in blood donation camps"
 }
+
+
+function slidecontent(){
+    const button = document.querySelector('.btn') 
+  for(let i=0;i<=2;i++){
+    if(i=0){
+        button.addEventListener("click", () =>{
+            document.getElementById('slidercontent').innerHTML = "Worked at Amazon for 2 years as front end developer"
+        })
+    if(i=1){
+        button.addEventListener("click", () =>{
+            document.getElementById('slidercontent').innerHTML = "Worked at Amazon for 3 years as front end developer"
+        })
+    if(i=2){
+        button.addEventListener("click", () =>{
+            document.getElementById('slidercontent').innerHTML = "Worked at Amazon for 4 years as front end developer"
+        })
+    i=0
+    }
+
+              
+    }
+}
+}
+}
